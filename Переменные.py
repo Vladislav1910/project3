91fcfad44676c636cf1f5b1e604734c05c501911
Vladislav1910 <vlad.makarenk@yandex.ru>
2024-08-12 14:26:21 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,8 @@
+number_of_completed = 12
+number_of_hours_spent = 1.5
+course = "Python"
+time_for_one_task = (number_of_hours_spent / number_of_completed)
+print('Курс:',course,','
+    ' Всего задач:',number_of_completed,','      #Доброго времени суток)) Я не понял как ставить запятые без пробела
+    ' Затраченных часов:',number_of_hours_spent,','
+    ' Среднее время выполнения:',time_for_one_task,'часа.')
